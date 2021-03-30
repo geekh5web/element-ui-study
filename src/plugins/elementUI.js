79085edf66072f42097aadb70b78a9cc73e6a743
wangwenhui <981837467@qq.com>
2021-03-30 16:53:44 +0800

    elementUI按需求引用成功，模板文件

---
+++ src/plugins/elementUI.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';//这行必须加，不然会报错
+import {
+     Button, 
+     Select,
+     Row,
+     Col,
+} from 'element-ui';
+
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Row)
+Vue.use(Col)
